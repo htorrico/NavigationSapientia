800b366053ea8689e9fad48c2c218ab85424cf40
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-03-12 09:56:25 -0500

    Add project files.

---
+++ App11/App11/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new Page1();
+            MainPage = new NavigationPage(new Page1());
+            //MainPage = new TabbedPage1();
+            //MainPage = new CarouselPage1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App11/App11/Page1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App11
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page2());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Message", "Yes", "No");
+        }
+    }
+}
